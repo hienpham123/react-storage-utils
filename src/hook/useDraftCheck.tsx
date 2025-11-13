e6dfde50395febbf3406f3bc1d9ea54679803b54
HienHo <hien.pham@bmbsoft.com.vn>
2025-11-13 15:12:56 +0700

    feat: update version and README

--- src/hook/useDraftCheck.tsx
+++ src/hook/useDraftCheck.tsx
@@ -1,41 +1,64 @@
 import * as React from "react";
 import { Dialog, DialogType, DialogFooter } from "@fluentui/react";
 import { DefaultButton, PrimaryButton } from "@fluentui/react";
-import { checkHasDraftInStorage } from "../Storage";
+import { checkHasDraftInStorage, getDraftFromStorage, jsonToFile, removeDraftFromStorage } from "../Storage";
 
 /** ---------- Types ---------- **/
 interface UseDraftCheckOptions {
     keys: string[];
-    onConfirm: (fromStorage: boolean) => void;
+    onConfirm: (data: Record<string, any> | null) => void;
+    condition?: boolean;
+    dependencies?: any[];
 }
 
 /** ---------- Hook ---------- **/
-export const useDraftCheck = ({ keys, onConfirm }: UseDraftCheckOptions) => {
-    const [isDialogVisible, setIsDialogVisible] = React.useState(false);
+export const useDraftCheck = ({ keys, onConfirm, condition = true, dependencies = [] }: UseDraftCheckOptions) => {
+    const [isDialogVisible, setIsDialogVisible] = React.useState<boolean>(false);
+    const [draftData, setDraftData] = React.useState<Record<string, any>>({});
 
     React.useEffect(() => {
         const check = async () => {
             try {
                 const hasDraft = await checkHasDraftInStorage(keys);
-                if (hasDraft) setIsDialogVisible(true);
-                else onConfirm(false);
+                const data: Record<string, any> = {};
+
+                for (const key of keys) {
+                    let value = await getDraftFromStorage(key);
+                    if (value && Object.hasOwn(value, 'data') && Object.hasOwn(value, 'type') && Object.hasOwn(value, 'name')) {
+                        value = await jsonToFile(value);
+                    }
+                    if (value !== null && value !== undefined) data[key] = value;
+                }
+
+                setDraftData(data);
+
+                if (hasDraft) {
+                    setTimeout(() => {
+                        setIsDialogVisible(true);
+                    }, 1000)
+                }
+                else onConfirm(null);
             } catch (err) {
                 console.error("checkHasDraftInStorage failed:", err);
-                onConfirm(false);
+                onConfirm(null);
             }
         };
-        check();
-    }, []);
+
+        if (condition) {
+            check();
+        }
+    }, [...dependencies]);
 
     const handleConfirm = React.useCallback(() => {
         setIsDialogVisible(false);
-        onConfirm(true);
-    }, [onConfirm]);
+        onConfirm(draftData);
+    }, [onConfirm, draftData]);
 
     const handleCancel = React.useCallback(() => {
         setIsDialogVisible(false);
-        onConfirm(false);
-    }, [onConfirm]);
+        onConfirm(null);
+        removeDraftFromStorage({ keysArr: keys })
+    }, [onConfirm, keys]);
 
     const dialog = (
         <Dialog
@@ -45,11 +68,12 @@ export const useDraftCheck = ({ keys, onConfirm }: UseDraftCheckOptions) => {
                 type: DialogType.normal,
                 title: "Restore data?",
                 closeButtonAriaLabel: "Close",
-                subText: "Do you want to restore the previously entered data before submitting/saving again?",
+                subText:
+                    "Do you want to restore the previously entered data before submitting/saving again?",
             }}
+            minWidth={450}
             modalProps={{
                 isBlocking: true,
-                styles: { main: { maxWidth: 450 } },
             }}
         >
             <DialogFooter>
